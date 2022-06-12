44f97004e5de4f3e8347bbfbc5ec45167120f4b3
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-12 20:45:15 +0300

    linking the displaying items for users API | Linking

---
+++ frontend-user/JS/display-items-user.js
@@ -0,0 +1,32 @@
+  window.onload = function () {
+    var item_display = document.getElementById("display_items");
+    axios({
+      method: "get",
+      url: "http://127.0.0.1:8000/api/v1/user/all-items",
+    }).then(function (response) {
+      var items = response.data.items;
+      console.log(items)
+      for (var i = 0; i < items.length; i++) {
+        console.log(items[i]);
+        const card = document.createElement("div");
+        card.classList.add('item-box');
+        card.innerHTML = `
+        <div class="item-image">
+          <img src="/frontend-user/assets/items/pants.jpg" alt="">
+        </div>
+        <div class="item-info">
+          <div class="item-text">
+            <h3>${items[i]["item_name"]}</h3>
+            <p>${items[i]["description"]}</p>
+            <p>${items[i]["price"]}</p>
+          </div>
+          <div class="item-like">
+            <i class="fa-solid fa-heart"></i>
+          </div>
+        </div>
+      `;
+        item_display.appendChild(card);
+      }
+    });
+  };
+  
