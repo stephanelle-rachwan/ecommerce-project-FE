f9857dcaf41dee9b38e7a59e3e92401a3cfbad58
Stephanelle Rachwan <stephanellerachwan@gmail.com>
2022-06-12 18:19:17 +0300

    linking the signup page | Linking

---
+++ frontend-user/JS/login.js
@@ -0,0 +1,8 @@
+let login = document.getElementById("signup");
+login.addEventListener("submit", function () {});
+  
+  let user_email = document.getElementById("user-email");
+  let user_password = document.getElementById("user-password");
+
+  data.append("email", user_email.value);
+  data.append("password", user_password.value);
---
+++ frontend-user/JS/signup.js
@@ -0,0 +1,25 @@
+let submit = document.getElementById("submit");
+  
+let user_name = document.getElementById("user-name");
+let user_email = document.getElementById("user-email");
+let user_password = document.getElementById("user_password");
+let confirm_password = document.getElementById("confirm_password");
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+  data=new FormData();
+  data.append("name", user_name.value);
+  data.append("email", user_email.value);
+  data.append("password", user_password.value);
+  data.append("password_confirmation", confirm_password.value);
+  data.append("role", 0);
+  
+  axios({
+    method: 'POST',
+    url:"http://127.0.0.1:8000/api/v1/user/register" ,
+    data:data,
+}).then(function (json) {
+  console.log(json);
+  console.log("hi");
+    window.location.href="login.html"
+})
+})
